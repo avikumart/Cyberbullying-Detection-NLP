cf7009cc6a5292fcc96639f3259ca97d6fe8daa7
Avikumar Talaviya <avikumar.talaviya@gmail.com>
2023-02-18 16:45:01 +0530

    file addition

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 # import python libraries
-import pandas
-import streamlit
+import pandas as pd
+import streamlit as st
+
+@st.cache
+def read_csv(file_path):
+    df = pd.read_csv(file_path)
+    return df
 
 
 # pos display
