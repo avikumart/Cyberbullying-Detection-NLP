4e863553b805294cc536ca156790bcc646a2683b
Avikumar Talaviya <avikumar.talaviya@gmail.com>
2023-02-22 18:31:02 +0530

    main page app

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ import nltk
 from nltk import word_tokenize
 import spacy
 nlp = spacy.load("en_core_web_sm")
+from textblob import TextBlob
+
+import gensim
+import pyLDAvis.gensim
+from gensim.models import word2vec
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk.stem.porter import PorterStemmer
+import joblib
 
 file_path = 'raw_text_file.csv'
 
@@ -102,13 +112,19 @@ The target variable is `oh-label` and the evaluation metric is `F1-score`.
 st.markdown("Please find GitHub repository link of project: [Click Here](https://github.com/avikumart/Cyberbullying-Detection-NLP)")
 
 # text input from user
-text_input = st.text_input("Enter the sample record of tweet", "A sample tweet")
+text_input = st.text_input("Enter the sample record of tweet", "A sample tweet text")
 
+# dataframe
 ndf = read_csv(file_path)
 
 # take input between maximum number of rows of dataframe
 select_num = st.number_input("Select number of first rows to choose for Named entity display",
                              min_value=10, max_value=16000)
+
+
+# take input of topic from users
+topic = st.selectbox("Select topics from below options",
+                     ('none','racism','sexism'))
 
 
 # NER display
@@ -121,6 +137,135 @@ def ner_df(n):
     ner_df = pd.DataFrame(ner_list, columns=['Word','Entity identification'])
     return ner_df
 
+# helper functions
+stopwrds = set(stopwords.words('english'))
+def remove_stowords(text, cores=2):
+    
+    sample = text
+    sample = sample.lower()
+    sample = [word for word in sample.split() if not word in stopwrds]
+    sample = ' '.join(sample)
+    
+    return sample
+
+# lemmatization function
+lemmatizer = WordNetLemmatizer()
+def lemma_clean_text(text, cores = 1):
+ 
+    sample = text
+    sample = sample.split()
+    sample = [lemmatizer.lemmatize(word.lower()) for word in sample]
+    sample = ' '.join(sample)
+    
+    return sample
+
+
+# wordnet_pos tagger
+def get_wordnet_pos(word):
+    
+    treebank_tag = nltk.pos_tag([word])[0][1]
+    
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return ''
+
+# porter stemmer object
+ps = PorterStemmer()
+
+# topic modeling function
+def preprocess_topic(df, topic):
+    """ Preprocessing function to model text data based on give topics.
+    
+    args:
+    df = input dataframe
+    topic = input topic "nonn", "sexism", or "racism"
+    
+    returns:
+    corpus of words under given topic
+    """
+    corpus=[]
+    # topic wise division
+    if topic == 'none':
+        for doc in ndf[ndf['Annotation'] == 'none']['cleaned_text']:
+            stop_word_removal = remove_stowords(doc)
+            lemmmatized_sample = lemma_clean_text(stop_word_removal)
+            words = lemmmatized_sample.split()
+            corpus.append(words)
+            
+    elif topic == 'sexism':
+        for doc in ndf[ndf['Annotation'] == 'sexism']['cleaned_text']:
+            stop_word_removal = remove_stowords(doc)
+            lemmmatized_sample = lemma_clean_text(stop_word_removal)
+            words = lemmmatized_sample.split()
+            corpus.append(words)
+                
+    elif topic == 'racism':
+        for doc in ndf[ndf['Annotation'] == 'racism']['cleaned_text']:
+            stop_word_removal = remove_stowords(doc)
+            lemmmatized_sample = lemma_clean_text(stop_word_removal)
+            words = lemmmatized_sample.split()
+            corpus.append(words) 
+            
+    return corpus
+
+
+# text correction function for modeling
+def correct_text(text, stem=False, lemma=False, spell=False):
+    if lemma and stem:
+        raise Exception('Either stem or lemma can be true, not both!')
+        return text
+    
+    sample = text
+    
+    #removing stopwords
+    sample = sample.lower()
+    sample = [word for word in sample.split() if word not in stops]
+    sample = ' '.join(sample)
+    
+    if lemma == True:
+        new_sample = []
+        sample = sample.split()
+        word_tags = [get_wordnet_pos(word.lower()) for word in sample]
+        for word in sample:
+            word_tag = word_tags[sample.index(word)]
+            if word_tag == "":
+                new_sample.append(lemmatizer.lemmatize(word))
+            else:
+                new_sample.append(lemmatizer.lemmatize(word.lower(), word_tag))
+        sample = ' '.join(new_sample)
+        
+    if stem == True:
+        sample = sample.split()
+        sample = [ps.stem(word) for word in sample]
+        sample = ' '.join(sample)
+    
+    if spell == True:
+        sample = str(TextBlob(text).correct())
+    
+    return sample
+
+
+# corpus of the words
+corpus = preprocess_topic(ndf, topic)
+
+# creat BOW model from corpus
+dic=gensim.corpora.Dictionary(corpus)
+bow_corpus = [dic.doc2bow(doc) for doc in corpus]
+
+# create LDA model using gensim library
+lda_model = gensim.models.LdaMulticore(bow_corpus,
+                                   num_topics = 4,
+                                   id2word = dic,
+                                   passes = 10,
+                                   workers = 2)
+
 # functions display display
 def main():
     text = text_cleaning(text_input)
@@ -132,15 +277,19 @@ def main():
     # NER run
     new_df = ner_df(select_num)
     st.dataframe(new_df)
+    
+    # topic display
+    st.dataframe(pd.DataFrame(lda_model.show_topics()))
+    
+    # model inference
+    correct_txt = correct_text(text)
+    tfidf = joblib.load('Models\tfidf_vectorizer.joblib')
+    model = joblib.load('Models\text_clf_model.joblib')
+    
+    vect_text = tfidf.transform(correct_text)
+    arr = vect_text.toarray()[0].reshape(1,-1)
+    prediction = model.predict(arr)
+    st.write(f"Prediction is: {prediction}")
         
 if __name__ == '__main__':
     main()
-
-
-    
-    
-# topic modeling display
-
-
-
-# text classification model inference
